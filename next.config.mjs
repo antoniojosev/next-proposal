57040f1afeb990a72716ab9b754631810c11f73f
Antonio <antoniovila.dev@gmail.com>
2025-09-18 05:03:51 -0400

    feat: configure Next.js settings, add deployment script, and update Tailwind CSS dark mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: '/next',  // Esto hará que funcione en antoniovila.dev/next
+  assetPrefix: '/next'
+};
 
 export default nextConfig;
